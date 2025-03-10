ddd6718d8ecaefb37c97cef5f4abab39ec0f1e68
hamish-leahy <ahamish265@gmail.com>
2025-03-11 07:27:21 +1100

    Update ima_efi.c

--- security/integrity/ima/ima_efi.c
+++ security/integrity/ima/ima_efi.c
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
+ * EVM EFI Secure Boot Handling
+ *
  * Copyright (C) 2018 IBM Corporation
  */
 #include <linux/efi.h>
@@ -11,45 +13,63 @@
 #define arch_ima_efi_boot_mode efi_secureboot_mode_unset
 #endif
 
+/**
+ * get_sb_mode - Retrieve the current secure boot mode
+ *
+ * Returns the current secure boot mode or efi_secureboot_mode_unknown
+ * if EFI runtime services are not supported.
+ */
 static enum efi_secureboot_mode get_sb_mode(void)
 {
 	enum efi_secureboot_mode mode;
 
 	if (!efi_rt_services_supported(EFI_RT_SUPPORTED_GET_VARIABLE)) {
-		pr_info("ima: secureboot mode unknown, no efi\n");
+		pr_info("ima: Secure boot mode unknown, EFI not supported\n");
 		return efi_secureboot_mode_unknown;
 	}
 
 	mode = efi_get_secureboot_mode(efi.get_variable);
-	if (mode == efi_secureboot_mode_disabled)
-		pr_info("ima: secureboot mode disabled\n");
-	else if (mode == efi_secureboot_mode_unknown)
-		pr_info("ima: secureboot mode unknown\n");
-	else
-		pr_info("ima: secureboot mode enabled\n");
+	switch (mode) {
+		case efi_secureboot_mode_disabled:
+			pr_info("ima: Secure boot mode disabled\n");
+			break;
+		case efi_secureboot_mode_unknown:
+			pr_info("ima: Secure boot mode unknown\n");
+			break;
+		case efi_secureboot_mode_enabled:
+			pr_info("ima: Secure boot mode enabled\n");
+			break;
+		default:
+			pr_err("ima: Invalid secure boot mode\n");
+			mode = efi_secureboot_mode_unknown;
+			break;
+	}
 	return mode;
 }
 
+/**
+ * arch_ima_get_secureboot - Check if secure boot is enabled
+ *
+ * Returns true if secure boot is enabled, false otherwise.
+ */
 bool arch_ima_get_secureboot(void)
 {
-	static enum efi_secureboot_mode sb_mode;
-	static bool initialized;
+	static enum efi_secureboot_mode sb_mode = efi_secureboot_mode_unset;
+	static bool initialized = false;
 
 	if (!initialized && efi_enabled(EFI_BOOT)) {
 		sb_mode = arch_ima_efi_boot_mode;
 
-		if (sb_mode == efi_secureboot_mode_unset)
+		if (sb_mode == efi_secureboot_mode_unset) {
 			sb_mode = get_sb_mode();
+		}
 		initialized = true;
 	}
 
-	if (sb_mode == efi_secureboot_mode_enabled)
-		return true;
-	else
-		return false;
+	return (sb_mode == efi_secureboot_mode_enabled);
 }
 
-/* secureboot arch rules */
+/* Secure boot architecture rules */
 static const char * const sb_arch_rules[] = {
 #if !IS_ENABLED(CONFIG_KEXEC_SIG)
 	"appraise func=KEXEC_KERNEL_CHECK appraise_type=imasig",
@@ -65,6 +85,12 @@ static const char * const sb_arch_rules[] = {
 	NULL
 };
 
+/**
+ * arch_get_ima_policy - Retrieve the IMA policy based on secure boot status
+ *
+ * Returns a pointer to the IMA policy rules if secure boot is enabled,
+ * NULL otherwise.
+ */
 const char * const *arch_get_ima_policy(void)
 {
 	if (IS_ENABLED(CONFIG_IMA_ARCH_POLICY) && arch_ima_get_secureboot()) {
